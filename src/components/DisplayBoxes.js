bd2e4dd489852526ab63a20e15b548c99829d753
TheSkinz <jwutsey@outlook.com>
2024-06-22 22:38:32 -0500

    Add DisplayBoxes component

---
+++ src/components/DisplayBoxes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DisplayBoxes = ({ distanceTraveled, remainingDistance }) => (
+  <div>
+    <div.info>Distance Traveled: {distanceTraveled} ft</div>
+    <div.info>Remaining Distance: {remainingDistance} ft</div>
+  </div>
+);
+
+export default DisplayBoxes;
